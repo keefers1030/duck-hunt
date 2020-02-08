c675b13e2351cd3fc0c91ab27b2ce689e2956383
John Carmichael <jc@stackeducation.com>
2020-02-08 10:42:52 -0500

    The solution to this exercise

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const scores = require('./scores')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+
+app.get('/', (request, response) => {
+  return response.render('index', { scores })
+})
+
+app.all('*', (request, response) => {
+  response.sendStatus(404)
+})
+
+app.listen(1337, () => {
+  console.log('Listening on port 1337...') // eslint-disable-line no-console
+})
